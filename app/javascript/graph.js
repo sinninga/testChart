ea3cc3e56497ac350383e6af550dbd8dbaf78d9a
Adam Sinning <asinning25@gmail.com>
2019-03-29 00:58:10 -0300

    chartjs added

---
+++ app/javascript/graph.js
@@ -0,0 +1,19 @@
+import Chart from 'chart.js';
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [50, 20, 30, 40, 50, 60]
+        }],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June']
+    },
+    options: {
+        showLines: true // disable for all datasets
+    }
+});
+
+
+
+export { myChart };
