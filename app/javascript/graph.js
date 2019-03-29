c3a2daf9cbf3261ce77aef19a7a97d3ed9fee858
Adam Sinning <asinning25@gmail.com>
2019-03-29 15:02:36 -0300

    containers added

--- app/javascript/graph.js
+++ app/javascript/graph.js
@@ -5,7 +5,7 @@ var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         datasets: [{
-            data: [50, 20, 30, 40, 50, 60]
+            data: [50, 20, 30, 40, 50, 60],
         }],
         labels: ['January', 'February', 'March', 'April', 'May', 'June']
     },
